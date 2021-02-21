61f040ebcc127db5c0e1bdd2771eec4d7e70fe0f
Rand Hale <randluca@gmail.com>
2021-02-21 11:45:01 -0800

    write initial component structure, trim extra React default files

--- clicker-game/src/App.js
+++ clicker-game/src/App.js
@@ -1,25 +1,21 @@
-import logo from './logo.svg';
+import React, { Component } from "react";
+import Wrapper from "./components/Wrapper";
+import ItemCard from "./components/ItemCard";
 import './App.css';
+import items from "./items.json";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  state = {
+    items: items
+  }
+
+  render() {
+    return (
+      <Wrapper>
+
+      </Wrapper>
+    )
+  }
 }
 
 export default App;
